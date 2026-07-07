70a7c525c937140e75a8068f86f971256a499dae
Dhananjay9834 <dhananjaybankar55@gmail.com>
2026-07-07 16:06:44 +0530

    Add files via upload

---
+++ 1. Operator/Assignment.java
@@ -0,0 +1,19 @@
+public class Assignment 
+{
+    public static void main(String args[])
+    {
+        int a=10,b=5,ans;
+        ans=a;
+        System.out.println(ans);//10
+        ans+=b;
+        System.out.println(ans);//15
+        ans-=a;
+        System.out.println(ans);//5
+        ans*=a;
+        System.out.println(ans);//50
+        ans/=b;
+        System.out.println(ans);//10
+        ans%=3;
+        System.out.println(ans);//1
+    }
+}
---
+++ 1. Operator/Relational.java
@@ -0,0 +1,13 @@
+public class Relational 
+{
+    public static void main(String args[])
+    {
+        int a=10,b=5;
+        System.out.println("10==5 "+(a==b));
+        System.out.println("10!=5 "+(a!=b));
+        System.out.println("10<5 "+(a<b));
+        System.out.println("10>5 "+(a>b));
+        System.out.println("10<=5 "+(a<=b));
+        System.out.println("10>=5 "+(a>=b));
+    }
+}
