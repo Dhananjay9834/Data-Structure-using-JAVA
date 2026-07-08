e4947353d49aff62d7f6379fb62b8b67d7ca7d48
Dhananjay Bankar <dhananjaybankar55@gmail.com>
2026-07-08 23:22:48 +0530

    Update IfElse.java

--- 2. Conditional Statements/IfElse.java
+++ 2. Conditional Statements/IfElse.java
@@ -1,21 +1,4 @@
 import java.util.*;
-/* EVEN/ODD number
-public class IfElse
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any numbers : ");
-        int A = sc.nextInt();
-        if(A%2==0){
-            System.out.println("Number is EVEN");
-        }else{
-            System.out.println("Number is ODD");
-        }  
-    }
-}
-*/
- //EVEN/ODD number
 public class IfElse
 {
     public static void main(String args[])
