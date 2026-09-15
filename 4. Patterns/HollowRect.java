8acdb096742320252b1efdb6e00aeb706e06ce0b
Dhananjay Bankar <183342812+Dhananjay9834@users.noreply.github.com>
2026-09-15 23:58:53 +0530

    Add files via upload

---
+++ 4. Patterns/HollowRect.java
@@ -0,0 +1,25 @@
+public class HollowRect
+{
+    public static void print(int rows,int cols)
+    {
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=cols;j++)
+            {
+                if(i==1 || i==rows || j==1 || j==cols)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        print(5,10);
+    }
+}
